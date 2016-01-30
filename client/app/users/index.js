fac0abdaff882ada1a368e3ca16a87655ed1aee9
Sergii Kamenskyi <sergii.kamenskyi@profitbricks.com>
2016-01-30 04:54:50 +0100

    user login and logout implemented

--- client/app/users/index.js
+++ client/app/users/index.js
@@ -2,56 +2,107 @@
 
 angular.module('myApp.users', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-        .when('/users', {
-            templateUrl: 'app/users/template.html',
-            controller: 'UsersCtrl'
-        })
-        .when('/users/register', {
-            templateUrl: 'app/users/registerForm.html',
-            controller: 'UsersRegisterCtrl'
-        })
-        .when('/users/login', {
-            templateUrl: 'app/users/loginForm.html',
-            controller: 'UsersLoginCtrl'
-        });
-}])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/users', {
+                templateUrl: 'app/users/template.html',
+                controller: 'UsersCtrl'
+            })
+            .when('/users/register', {
+                templateUrl: 'app/users/registerForm.html',
+                controller: 'UsersRegisterCtrl'
+            })
+            .when('/users/login', {
+                templateUrl: 'app/users/loginForm.html',
+                controller: 'UsersLoginCtrl'
+            });
+    }])
+    .factory('userApi', ['$resource', function($resource) {
+        return $resource('/api/v1/users/:userId');
+    }])
+    .factory('authApi', ['$resource', function($resource) {
+        return $resource('/api/v1/users/auth');
+    }])
+    .factory('currentUserModel', ['authApi', function(authApi) {
+        var user = {
+            properties: {},
+            setProperties: function(obj) {
+                this.properties = obj;
+            },
 
-.controller('UsersCtrl', ['$scope', '$resource', '$location', function($scope, $resource, $location) {
-        // User
-        var User = $resource('/api/v1/users/:userId');
+            logout: function() {
+                authApi.delete();
+                this.setProperties({})
+            },
 
+            isLoggedIn: function() {
+                return !!this.properties.email
+            },
+            query: function(cb) {
+                cb = cb || function(err, data) {
+                    };
+
+                authApi.get(function(res) {
+                    user.setProperties(res);
+                    cb(null, res);
+                }, cb)
+            }
+        };
+
+        user.query();
+
+        return user;
+    }])
+    .controller('UsersCtrl', ['$scope', 'userApi', 'authApi', '$location', function($scope,
+                                                                                    userApi,
+                                                                                    authApi,
+                                                                                    $location) {
         // We can retrieve a collection from the server
-        User.query(function(res) {
-            console.log(res)
+        userApi.query(function(res) {
             $scope.users = res;
         });
 
         $scope.logout = function() {
-            var Auth = $resource('/api/v1/users/auth');
-            Auth.delete($scope.form, function(user, param) {
+            authApi.delete($scope.form, function(user, param) {
                 $location.path('/users')
             })
         }
     }])
-    .controller('UsersRegisterCtrl', ['$scope', '$resource', '$location', function($scope, $resource, $location) {
-        var User = $resource('/api/v1/users/:userId');
+    .controller('UsersRegisterCtrl', ['$scope', 'userApi', '$location', function($scope, userApi, $location) {
         $scope.submit = function() {
-            console.log($scope.form)
-            User.save($scope.form, function(user, param) {
-                console.log('saved', user, param)
+            userApi.save($scope.form, function(user, param) {
                 $location.path('/users')
             })
         }
     }])
-
-.controller('UsersLoginCtrl', ['$scope', '$resource', '$location', function($scope, $resource, $location) {
-    var Auth = $resource('/api/v1/users/auth');
-    $scope.submit = function() {
-        Auth.save($scope.form, function(user, param) {
-            console.log('saved', user, param)
-            $location.path('/users')
-        })
-    }
-}]);
+    .controller('UsersLoginCtrl', ['$scope', 'authApi', '$location', 'currentUserModel', function($scope,
+                                                                                                  authApi,
+                                                                                                  $location,
+                                                                                                  currentUserModel) {
+        $scope.submit = function() {
+            $scope.error = false;
+            authApi.save($scope.form, function(user, param) {
+                    $scope.error = false;
+                    currentUserModel.setProperties(user);
+                    $location.path('/users')
+                }, function() {
+                    $scope.error = true;
+                }
+            )
+        }
+    }])
+    .directive('userStatus', ['currentUserModel', function(currentUserModel) {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {},
+            link: function($scope, $element, $attributes) {
+                $scope.user = currentUserModel;
+                $scope.run = function() {
+                    console.log(currentUserModel);
+                }
+            },
+            templateUrl: 'app/users/statusControl.html'
+        }
+    }])
+;
